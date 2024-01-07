d579c5f92fc5a67315b9ce0924b7874097e61336
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-07 01:03:55 +0000

    Update

--- src/main/java/com/websocket/Websocket/Greeting.java
+++ src/main/java/com/websocket/Websocket/Greeting.java
@@ -1,4 +1,19 @@
 package com.websocket.Websocket;
 
+
 public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
